3f9dcdb27141cddd40c7f3a2c640fd43463e5742
tjforeman <tylerforeman92@gmail.com>
2019-05-19 19:30:01 -0400

    added knex migration for guides, route and crud ops for guides, and get users by id with guides rendering inside user object

--- data-routes/users-route.js
+++ data-routes/users-route.js
@@ -10,4 +10,25 @@ const protected=require('../protected/protected.js')
       .catch(err => res.send(err));
   });
 
+  router.get('/:id', protected, (req,res)=>{
+    db('users')
+    .where({id:req.params.id})
+    .first()
+    .then(user=>{
+        if(user){
+        db('guides')
+        .where({user_id:req.params.id})
+        .then(guides=>{
+          user.guides=guides;
+          res.status(200).json(user)
+        })
+      }else{
+          res.status(404).json({message:'The User with the Specified ID does not exist'})
+      }
+    })
+    .catch(err=>{
+        res.status(500).json({error:err,message:'Unable to find the Specified User at this time.'})
+    })
+})
+
    module.exports=router 
---
+++ data/migrations/20190519162528_create_guides.js
@@ -0,0 +1,51 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('guides', tbl => {
+    tbl.increments();
+
+    tbl
+   .string('title', 128)
+   .notNullable()
+   .unique();
+
+   tbl
+    .integer('user_id')
+    .unsigned()
+    .references('id')
+    .inTable('users')
+    .onDelete('CASCADE')
+    .onUpdate('CASCADE')
+
+   tbl
+   .string('type',128)
+   .notNullable()
+
+    tbl
+   .string('description',255)
+   .notNullable();
+
+   tbl
+   .string('step_1')
+   .notNullable();
+
+   tbl
+   .string('step_2')
+   
+
+   tbl
+   .string('step_3')
+   
+
+   tbl
+   .string('step_4')
+
+   tbl
+   .string('step_5')
+
+    });
+};
+
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+};
